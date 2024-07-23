d8208f5fd369c87a2ff8bb4f9f56da69cd001c1e
Oganes <og.marckaryan@yandex.ru>
2024-07-23 11:35:32 +0300

    fix conf for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry:'./src/index.js',
     output:{
         filename:"bundle-prod.js",
-        path:path.resolve(__dirname,'dist'),
+        path:path.resolve(__dirname,'prod'),
     },
     module: {
         rules: [
