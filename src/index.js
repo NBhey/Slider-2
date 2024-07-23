95933d341965304d62bad58caf90602a071d4255
Oganes <og.marckaryan@yandex.ru>
2024-07-23 11:27:25 +0300

    fin slider with config loop/autoplay/interval

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
-import './style.css'
+import "./style.css";
+import createCarousel from "./js/slider.js";
 
-const slider = document.querySelector('.slider')
+
+const slider = document.querySelector(".slider");
+
 const carousel = {
-    autoPlay: true,
-    timer: 1000,
-    loop: false
-}
+  autoPlay: true,
+  intreval: 5000,
+  loop: true,
+};
 
-function createCarousel(el, param){
-    el = el.children;
-    console.log(el.length)
-    let currentSlide = 0;
-    el[currentSlide].style.display = "block";
-  
-    const nextBtn = document.getElementById("next");
-    function nextSlide() {
-      currentSlide += 1;
-      currentSlide >= el.length ? (currentSlide = 0) : false;
-      
-      for (let i = 0; i < el.length; i++) {
-        el[i].style.display = "none";
-      }
-      el[currentSlide].style.display = "block";
-    }
-    nextBtn.addEventListener("click", nextSlide);
 
-    if(param.autoPlay === true){
-       let interval = setInterval(nextSlide, param.timer) 
-    }
-    console.log('123')
-    if(param.loop === false && currentSlide === 3){
-      clearInterval(interval)
-    } 
-}
-
-createCarousel(slider,carousel)
+createCarousel(slider, carousel);
